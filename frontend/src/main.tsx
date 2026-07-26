f7d59fab39ec5395b08c3c6b13e1d576571640ac
junjiax <tuangiai52@gmail.com>
2026-07-26 10:27:53 +0700

    fixed: error  Use @ts-expect-error instead of @ts-ignore, as @ts-ignore will do nothing if the following line is error-free  @typescript-eslint/ban-ts-comment

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-// @ts-ignore
-import ErrorBoundary from './components/ErrorBoundary.jsx'
+// //@ts-expect-error
+// import ErrorBoundary from './components/ErrorBoundary.jsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary>
       <App />
-    </ErrorBoundary>
   </StrictMode>,
 )
